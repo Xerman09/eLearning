4de7224006ce4f93bc71e0c76171dd263143170d
Erman <66209038+Xerman09@users.noreply.github.com>
2025-04-23 22:39:27 +0800

    Update practiceExam.js
    Updating the link for CSE

--- practiceExam.js
+++ practiceExam.js
@@ -2,7 +2,7 @@
 
 const practiceExamArray = [
     {
-      link: "./library/practiceQuiz.html?subject=cse_question",
+      link: "./library/practiceExam.html?subject=cse_question",
       subjectTitle: "Civil Service Exam",
     },
     ];
