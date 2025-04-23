52a237815acc79e995dca1bc85d915c88fbfc5c9
Erman <66209038+Xerman09@users.noreply.github.com>
2025-04-23 19:42:24 +0800

    Update subjectHomeInfo.js

--- asset/js/subjectHomeInfo.js
+++ asset/js/subjectHomeInfo.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const subjectList = [
     {
         code : 'differentialEquation', 
@@ -14,21 +14,4 @@ const subjectList = [
             },
         ]
     }
-=======
-const subjectList = [
-    {
-        code : 'differentialEquation', 
-        subjectTitle : 'Differential Equation',
-        description : '&emsp; &emsp; Differential Equations is a branch of mathematics that deals with equations involving derivatives, which describe how a quantity changes with respect to another. These equations express relationships between a function and its derivatives, and are fundamental in modeling a wide range of real-world phenomena such as population growth, heat transfer, electrical circuits, and motion. Differential equations are classified into two main types: ordinary differential equations (ODEs), which involve derivatives with respect to a single variable, and partial differential equations (PDEs), which involve derivatives with respect to multiple variables. Solving differential equations involves finding functions that satisfy the given relationships, and these solutions provide insights into dynamic systems in fields like physics, engineering, biology, and economics.',
-        listOfLecture : [
-            {
-                code: 'de_lec3',
-                lectureNo : '3',
-                lectureTitle: 'Introduction to Families of Curve',
-                lectureFolderName: '../images/de_lecture_3/3. Families of Curves_page-',
-                link: '../library/lecture.html?subject=differentialEquation&lectureCode=de_lec3'
-            },
-        ]
-    }
->>>>>>> 280ec2de12fadaf9bdbe8e3614b3e5f255b68551
 ]
